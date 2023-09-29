6ca9ab95a632ad30337308e58f1729e344169354
scRaul <61263078+scRaul@users.noreply.github.com>
2023-09-29 14:17:05 -0700

    UI Fix
    allow to exit detail when clicking outside or modal

--- components/drinkInfo.tsx
+++ components/drinkInfo.tsx
@@ -5,14 +5,14 @@ import { ArrowBigLeft } from "lucide-react";
 
 const DrinkInfo = ({ drink, onClose }: { drink: Drink; onClose: any }) => {
   return (
-    <div className="detail-modal">
+    <div className="detail-modal" onClick={onClose}>
       <Card>
-        <div className="top-card">
+        <div className="top-card" onClick={(e) => e.stopPropagation()}>
           <ArrowBigLeft size={32} className="bttn" onClick={onClose} />
           <h2 className="title">{drink.name}</h2>
           <div className="spacer"></div>
         </div>
-        <div className="modal-content">
+        <div className="modal-content"  onClick={(e) => e.stopPropagation()}>
           <ul className="list">
             {drink.ingredientList.map((ingredient, index) => (
               <li className="item" key={index}>
